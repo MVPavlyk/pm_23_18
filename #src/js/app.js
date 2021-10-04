1b8575372efee5ed9127281adf9320a1b5549c53
MVPavlyk <grandbityyy67@gmail.com>
2021-10-04 18:01:59 +0300

    ws test

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
 	$('.burger_menu').click(function(event){
-		$('.nav, .ann, .left_b_divs, .messages, .courses').toggleClass('active');
+		$('.nav, .ann, .left_b_divs, .messages, .courses, .nav_text, .nav_point').toggleClass('active');
 	});
 });
